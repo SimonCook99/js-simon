4ffbea6c06d6fd731c1cbe4b1522efa36e6a9a04
Simone Mauro <simon.mauro@virgilio.it>
2022-02-07 16:46:12 +0100

    Inizio timeout

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+let numberContainer = document.getElementById("number-container");
+
+//array che conterrà la lista dei numeri random
 let listaNumeri = [];
 
+//ciclo che aggiunge i numeri e aggiorna l'html
 for(let i = 0; i < 5; i++){
     let numero = randomRange(1, 99);
 
@@ -7,22 +11,52 @@ for(let i = 0; i < 5; i++){
         listaNumeri.push(numero);
     }
 
-    document.getElementById("number-container").innerHTML += listaNumeri[i]+ ",";
+    if(i == 4){
+        numberContainer.innerHTML += listaNumeri[i];
+    }else{
+        numberContainer.innerHTML += listaNumeri[i]+ ",";
+    }
 }
 
 console.log(listaNumeri);
 
 
+setTimeout(inserisciNumeri, 3000);
 
 
+//funzione che verrà chiamata dopo i 30 secondi d'attesa
+function inserisciNumeri(){
+    numberContainer.innerHTML = "";
 
+    //array che conterrà i numeri che l'utente indovinerà
+    let numeriIndovinati = [];
+    for(let i = 0; i < 5; i++){
+        let valore = parseInt(prompt("Inserisci un numero"));
+        console.log(valore);
 
+        while(isNaN(valore)){
+            valore = prompt("Inserisci un numero VALIDO!!!");
+        }
 
+        if(listaNumeri.includes(valore)){
+            numeriIndovinati.push(valore);
+            
+        }
+    }
 
+    console.log(numeriIndovinati);
 
-
-
-
+    //ultimo ciclo che scorre l'array dei numeri indovinati, e aggiorna l'html per mostrare all'utente
+    for(let j = 0; j < numeriIndovinati.length; j++){
+        if(j == numeriIndovinati.length - 1){
+            numberContainer.innerHTML += numeriIndovinati[j];
+        }else{
+            numberContainer.innerHTML += numeriIndovinati[j] + ",";   
+        }
+    }
+    
+    
+}
 
 //funzione che torna un numero random di un range predefinito
 function randomRange(min, max){
