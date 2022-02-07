2dda562c0bc65cecc5ca26a2104d5dd816adb20d
Simone Mauro <simon.mauro@virgilio.it>
2022-02-07 18:02:20 +0100

    esercizio completato

--- main.js
+++ main.js
@@ -40,37 +40,55 @@ function inserisciNumeri(){
     //array che conterrà i numeri che l'utente indovinerà
     let numeriIndovinati = [];
 
-    for(let i = 0; i < 5; i++){
-        let valore = parseInt(prompt("Inserisci un numero"));
-        console.log(valore);
-
-        while(isNaN(valore)){
-            valore = prompt("Inserisci un numero VALIDO!!!");
-        }
-
-        if(listaNumeri.includes(valore)){
-            numeriIndovinati.push(valore);
-        }
-
-        /* if(numeriIndovinati.includes(valore)){
-            alert("VALORE GIA' PRESENTE");
-        } */
-
-    }
-
-    console.log(numeriIndovinati);
-
-    //ultimo ciclo che scorre l'array dei numeri indovinati, e aggiorna l'html per mostrare all'utente
-    for(let j = 0; j < numeriIndovinati.length; j++){
-        if(j == numeriIndovinati.length - 1){
-            numberContainer.innerHTML += numeriIndovinati[j];
-        }else{
-            numberContainer.innerHTML += numeriIndovinati[j] + ",";   
-        }
-    }
+    //leggero timeout di 250 millisecondi per permettere ai numeri random di scomparire prima di chiedere i prompt
+    setTimeout(function(){
+        for(let i = 0; i < 5; i++){
+            let valore = parseInt(prompt("Inserisci un numero"));
+            console.log(valore);
     
+            controllaValidità(valore);
+    
+            if(listaNumeri.includes(valore)){
+                
+                //questo while controlla se il valore inserito, non sia già stato inserito in precedenza
+                while(numeriIndovinati.includes(valore)){
+                    valore = parseInt(prompt("VALORE GIA' PRESENTE, inserisci un alro numero"));
+                    controllaValidità(valore);
+                }
+    
+                if(!numeriIndovinati.includes(valore)){
+                    numeriIndovinati.push(valore);
+                    console.log("ho inserito il numero" + valore);
+                }
+                
+                console.log(numeriIndovinati);
+            }
+    
+        }
+    
+        document.getElementById("descrizione").innerHTML = "Hai indovinato " + numeriIndovinati.length + " numeri";
+    
+        console.log(numeriIndovinati);
+    
+        //ultimo ciclo che scorre l'array dei numeri indovinati, e aggiorna l'html per mostrare all'utente
+        for(let j = 0; j < numeriIndovinati.length; j++){
+            if(j == numeriIndovinati.length - 1){
+                numberContainer.innerHTML += numeriIndovinati[j];
+            }else{
+                numberContainer.innerHTML += numeriIndovinati[j] + ",";   
+            }
+        }
+    }, 250);
     
 }
+
+
+function controllaValidità(numero){
+    while(isNaN(numero)){
+        numero = prompt("Inserisci un numero VALIDO!!!");
+    }
+}
+
 
 //funzione che torna un numero random di un range predefinito
 function randomRange(min, max){
